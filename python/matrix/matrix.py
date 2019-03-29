e9858444c75cf920a3703b61d57ee573921d2f5a
Guillaume Martin <ge.martin74@gmail.com>
2019-03-29 14:46:24 +0800

    completes row() method

---
+++ python/matrix/matrix.py
@@ -0,0 +1,10 @@
+class Matrix(object):
+    def __init__(self, matrix_string):
+        self.matrix_string = matrix_string
+
+    def row(self, index):
+        row = list(map(int, self.matrix_string.split('\n')[index - 1].split(' ')))
+        return row
+
+    def column(self, index):
+        pass
