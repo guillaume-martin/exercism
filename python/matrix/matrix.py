2e681878df32c6d1c742990d0d3b17e58e7f39ed
Guillaume Martin <ge.martin74@gmail.com>
2019-04-15 15:35:48 +0800

    improved matrix solution

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,13 +1,17 @@
 class Matrix(object):
     def __init__(self, matrix_string):
         self.matrix_string = matrix_string
+        self.rows = [list(map(int, row.split(' '))) for row in matrix_string.split('\n')]
+        nrow = len(self.rows)
+        ncol = len(self.rows[0])
+        self.columns = []
+        for i in range(ncol):
+            self.columns.append([self.rows[j][i] for j in range(nrow)])
 
     def row(self, index):
-        row = list(map(int, self.matrix_string.split('\n')[index - 1].split(' ')))
-        return row
+        return self.rows[index - 1]
 
     def column(self, index):
-        column = [int(row.split(' ')[index - 1]) for row in self.matrix_string.split('\n')]
-        return column
+        return self.columns[index - 1]
 
 
