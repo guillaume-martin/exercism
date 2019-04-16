23a3cf85dec4d49484cdf227fcbd54ec84c9f043
Guillaume Martin <ge.martin74@gmail.com>
2019-04-16 13:58:24 +0800

    build rows with nested list comprehension

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,7 +1,7 @@
 class Matrix(object):
     def __init__(self, matrix_string):
         self.matrix_string = matrix_string
-        self.rows = [list(map(int, row.split(' '))) for row in matrix_string.split('\n')]
+        self.rows = [[int(r) for r in row.split(' ')] for row in matrix_string.splitlines()]
         nrow = len(self.rows)
         ncol = len(self.rows[0])
         self.columns = []
