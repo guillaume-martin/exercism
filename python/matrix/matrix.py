694a0f652c78b0ee8b3163a67b0cd3c0d53ca0a5
Guillaume Martin <ge.martin74@gmail.com>
2019-04-17 14:25:02 +0800

    removed for loop from matrix solution

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -2,17 +2,10 @@ class Matrix(object):
     def __init__(self, matrix_string):
         self.matrix_string = matrix_string
         self.rows = [[int(r) for r in row.split(' ')] for row in matrix_string.splitlines()]
-
-        ncol = len(self.rows[0])
-
-        self.columns = []
-        for i in range(ncol):
-            self.columns.append([z[0][i] for z in zip(*[self.rows])])
+        self.columns = [list(tpl) for tpl in list(zip(*self.rows))]
 
     def row(self, index):
         return self.rows[index - 1]
 
     def column(self, index):
         return self.columns[index - 1]
-
-
