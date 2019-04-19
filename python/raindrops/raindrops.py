d700ae57eecd924bb1793bba837d9ee8281f902c
Guillaume Martin <ge.martin74@gmail.com>
2019-04-19 13:45:16 +0800

    solved raindrop

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,12 +1,5 @@
 def raindrops(number):
-    factors = {
-        '3':'Pling',
-        '5':'Plang',
-        '7':'Plong'
-    }
-    output = ''
-    for factor in [3, 5, 7]:
-        if number%factor == 0:
-            output += factors[str(factor)]
-
+    factors = {'3': 'Pling', '5': 'Plang', '7': 'Plong'}
+    output = ''.join([factors[str(f)] for f in [3, 5, 7] if number % f == 0])
     return output if len(output) > 0 else str(number)
+
