663d518ccd2a7fd97ac688c45abbfb1c7d9769aa
Guillaume Martin <ge.martin74@gmail.com>
2019-04-26 14:12:37 +0800

    first working solution

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,7 +1,8 @@
 def verify(isbn):
+
     try:
         digits = list(isbn.replace('-', ''))
-
+        print(digits)
         # isbn has 10 digits
         assert len(digits) == 10
 
@@ -9,13 +10,22 @@ def verify(isbn):
         assert sum(int(d) in range(0, 10) for d in digits[:9]) == 9
 
         # the last position is either a number or an 'X'
-        assert int(digits[9]) in range(0, 10) or digits[9] == 'X'
+        if digits[9] != 'X':
+            assert int(digits[9]) in range(0, 10)
 
-        return True if  sum(int(d) * m for d, m in zip(digits, range(11, 0, -1)))%11 == 0 else False
-    
+        # replace X with 10
+        if 'X' in digits:
+            digits[digits.index('X')] = 10
+        print(digits)
+        return True if  sum(int(d) * m for d, m in zip(digits, range(10, 0, -1)))%11 == 0 else False
+
     except AssertionError:
         print('Not a valid ISBN')
         return False
 
     except ValueError:
+        print('ValueError')
         return False
+
+
+print(verify('359821507X'))
