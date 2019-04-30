eaec7b7c3dde5cc3b0febf176a34090aa126dde0
Guillaume Martin <ge.martin74@gmail.com>
2019-04-30 13:35:19 +0800

    improved isogram solution

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,3 +1,3 @@
 def is_isogram(string):
     string = string.replace(' ', '').replace('-', '').lower()
-    return True if len(string) == len(set(string)) else False
+    return len(string) == len(set(string))
