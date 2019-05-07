d7a4cb2d7aa43b2d400bdb8c248af01ae105cfec
Guillaume Martin <ge.martin74@gmail.com>
2019-05-07 13:47:54 +0800

    twelve_days first working solution

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,19 +1,18 @@
+import re
+
 def verify(isbn):
 
+    regex = '[0-9]{9}([0-9]|X)'
+
     try:
-        digits = list(isbn.replace('-', ''))
-
-        # isbn has 10 digits
-        assert len(digits) == 10
-
-        # first 9 positions are numbers
-        assert sum(int(d) in range(0, 10) for d in digits[:9]) == 9
+        # control format
+        isbn = isbn.replace('-', '')
+        re.match(regex, isbn)
 
         # replace X with 10
-        if 'X' in digits:
-            digits[digits.index('X')] = 10
+        isbn = isbn.replace('X', '')
 
-        return True if  sum(int(d) * m for d, m in zip(digits, range(10, 0, -1)))%11 == 0 else False
+        return True if  sum(int(d) * m for d, m in zip(list(isbn), range(10, 0, -1)))%11 == 0 else False
 
     except:
         return False
