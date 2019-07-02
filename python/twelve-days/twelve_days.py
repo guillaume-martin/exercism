32fa0faeea0111b8c323b8384ce0c417fc419f7b
Guillaume Martin <ge.martin74@gmail.com>
2019-07-02 13:38:30 +0800

    improved twelve_days solution

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,17 +1,55 @@
-def recite(start_verse, end_verse):
-    verses = {
-        1: 'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.',
-        2: 'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.',
-        3: 'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-        4: 'On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-        5: 'On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-        6: 'On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-        7: 'On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-        8: 'On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-        9: 'On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-        10: 'On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-        11: 'On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-        12: 'On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
+def gift_list(number):
+    """Generates the list of gifts for a given verse
+
+    Parameters
+    ----------
+    number: Integer
+        The number of the verse we want the list for
+
+    Returns
+    -------
+    string
+        The list of gifts
+    """
+    gifts = {
+        1: 'a Partridge in a Pear Tree',
+        2: 'two Turtle Doves',
+        3: 'three French Hens',
+        4: 'four Calling Birds',
+        5: 'five Gold Rings',
+        6: 'six Geese-a-Laying',
+        7: 'seven Swans-a-Swimming',
+        8: 'eight Maids-a-Milking',
+        9: 'nine Ladies Dancing',
+        10: 'ten Lords-a-Leaping',
+        11: 'eleven Pipers Piping',
+        12: 'twelve Drummers Drumming'
     }
 
-    return [''.join(verses[number]) for number in range(start_verse, end_verse + 1)]
+    return gifts[1] if number == 1 \
+           else ', '.join([gifts[n] for n in reversed(range(2, number+1))]) + \
+           f', and {gifts[1]}'
+
+def recite(start_verse, end_verse):
+    """ Generates a list of verses from the song Twelve days of Christmas
+
+    Parameters
+    ----------
+    start_verse: Integer
+        The number of the first verse to recite
+
+    end_verse: Integer
+        The number of the last verse to recite
+
+    Returns
+    -------
+    list
+        A list of verses from start_verse to end_verse
+    """
+
+    ordinals = {1:'first', 2:'second', 3:'third', 4:'fourth', 5:'fifth', 6:'sixth',
+                7:'seventh', 8:'eighth', 9:'ninth', 10:'tenth', 11:'eleventh',
+                12:'twelfth'}
+
+    return [f"On the {ordinals[num]} day of Christmas my true love gave to me: {gift_list(num)}."
+            for num in range(start_verse, end_verse + 1) ]
