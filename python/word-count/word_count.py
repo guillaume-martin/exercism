7160ebcbac9e3ee1e4327359c96f870d01ead87c
Guillaume Martin <guillaume@enspyre.com>
2019-08-29 16:28:10 +0800

    improved word_count solution

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,17 +1,13 @@
 import re
+from collections import Counter
+
 def count_words(sentence):
     word_count = {}
 
     sentence = re.sub('[,_-]', ' ', sentence)
 
-    words_list = [re.sub('[^A-Za-z0-9\']+', '', word.strip("'")) for word in sentence.lower().split()]
+    words_list = [w for w in [re.sub('[^A-Za-z0-9\']+', '', word.strip("'")) for word in sentence.lower().split()] if len(w) > 0]
+    counter = Counter(words_list)
 
-    for word in words_list:
-        if len(word) > 0:
-            try:
-                word_count[word] += 1
-            except KeyError:
-                word_count[word] = 1
-
-    return word_count
+    return counter
 
