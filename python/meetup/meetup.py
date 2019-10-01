7c81745ed0afaed2786e3b21a751d18ac4551cc3
Guillaume Martin <guillaume@enspyre.com>
2019-10-01 13:59:26 +0800

    improves meetup solution using itermonthdays2

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,5 +1,5 @@
 import datetime
-from calendar import monthrange
+import calendar
 
 
 class MeetupDayException:
@@ -7,23 +7,20 @@ class MeetupDayException:
 
 
 def meetup(year, month, week, day_of_week):
-
+    cal = calendar.Calendar()
     weekdays = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3,
                 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
+    month_days = cal.itermonthdays2(year, month)
+    days = [day for day, weekday in month_days if weekday == weekdays[day_of_week] and day > 0]
 
-    if week == 'teenth':
-        day = [d for d in range(13, 20)
-               if datetime.date(year, month, d).weekday() == weekdays[day_of_week]][0]
-    else:
-        last_day = monthrange(year, month)[1]
-        list_of_dates = [day for day in range(1, last_day + 1)
-                         if datetime.date(year, month, day).weekday() == weekdays[day_of_week]]
-        if week == 'last':
-            day = list_of_dates[-1]
+    try:
+        if week == 'teenth':
+            day = [d for d in days if d in range(13, 20)][0]
+        elif week == 'last':
+            day = days[-1]
         else:
-            try:
-                day = list_of_dates[int(week[0]) - 1]
-            except IndexError:
-                raise MeetupDayException
+            day = days[int(week[0]) - 1]
+    except Exception:
+        raise MeetupDayException
 
     return datetime.date(year, month, day)
